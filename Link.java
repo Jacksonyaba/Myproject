85d9a82774f6c31bef33dcaadc761d5486bbb8a6
Jackson <119449451+Jacksonyaba@users.noreply.github.com>
2023-01-14 22:27:01 +0300

    Add files via upload

---
+++ Link.java
@@ -0,0 +1,64 @@
+
+package MyProject;
+
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "link")
+@SessionScoped
+public class Link implements Serializable{
+    private static final long serialVersionUID = -7250065889869767422L;
+    
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+   
+    public void validateEmail(FacesContext context, UIComponent comp,Object value) {
+        System.out.println("inside validate method");
+        String email = (String) value;
+        
+        
+    }
+    
+     public void validatePassword(FacesContext context, UIComponent comp,Object value) {
+        System.out.println("inside validate method");
+        String password = (String) value;
+        
+        if(password.length()<4){
+            ((UIInput) comp).setValid(false);
+            FacesMessage message = new FacesMessage(
+            "Minimum length of password is 4");
+            context.addMessage(comp.getClientId(context), message);
+        }
+        
+     }
+     public String doLogin(){
+       
+       return "HomePage";
+}
+  
+   }
+      
+   
+
